642d862f4203442fd7c25fa1599de0c2f8e8d792
Joao <jp.bruno.machado@gmail.com>
2022-08-18 14:55:29 -0300

    fixed shouldForwardProp

--- components/ItemAnuncio/ItemAnuncio.tsx
+++ components/ItemAnuncio/ItemAnuncio.tsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import isPropValid from '@emotion/is-prop-valid';
 
 type AnuncioProps = {
     imgSrc: string,
@@ -23,23 +24,31 @@ const Anuncio = styled.div`
     }
 `;
 
-const Imagem = styled('div', { shouldForwardProp:  prop => prop === 'imgSrc'})((props: Partial<AnuncioProps>) => ({
+const ImagemStyled = styled('div', { shouldForwardProp: prop => isPropValid(prop) && prop !== 'imgSrc' })((props:Partial<AnuncioProps>) => ({
     position: "relative",
     minWidth: "162px",
     minHeight: "121px",
     borderRadius: "10px",
     overflow: "hidden",
-    backgroundColor: 'rgb(229, 229, 229)',
+    backgroundColor: "rgb(229, 229, 229)",
     backgroundImage: `url(${props.imgSrc})`,
-    backgroundSize: 'auto 100%',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
+    backgroundSize: "auto 100%",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center"
+
 }))
+
+function Imagem({ imgSrc }:Partial<AnuncioProps>) {
+    return (
+        <ImagemStyled imgSrc={imgSrc} />
+    )
+}
+
 
 export default function ItemAnuncio({ imgSrc, titulo, valor }: AnuncioProps) {
     return (
         <Anuncio>
-            <Imagem imgSrc={imgSrc}/>
+            <Imagem imgSrc={imgSrc} />
             <h4>{titulo}</h4>
             <span>R$ {valor}</span>
         </Anuncio>
